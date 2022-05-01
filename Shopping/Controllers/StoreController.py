a1d7f988c1356c78bb6b1f23428d39237c264b2e
Caden Austin <cadenbryant@hotmail.com>
2022-05-01 06:54:00 -0400

    Fixed tuple type hinting

--- Shopping/Controllers/StoreController.py
+++ Shopping/Controllers/StoreController.py
@@ -1,11 +1,11 @@
 from Helpers import getSession
 from Models import Book, InventoryItem, Movie
 
-def getAllBooks() -> list[tuple(InventoryItem, Movie)]:
+def getAllBooks() -> list[tuple[InventoryItem, Movie]]:
     session = getSession()
     return session.query(InventoryItem, Book).join(Book, InventoryItem.book_reference == Book.id).all()
 
-def getAllMovies() -> list[tuple(InventoryItem, Movie)]:
+def getAllMovies() -> list[tuple[InventoryItem, Movie]]:
     session = getSession()
     return session.query(InventoryItem, Movie).join(Movie, InventoryItem.book_reference == Movie.id).all()
 
